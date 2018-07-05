fc1195c336396784dcd855910a10088c49f7fef6
Ed Thomas <et1270@my.bristol.ac.uk>
2018-07-05 12:04:29 +0100

    Exercises 1&2

---
+++ exercises.rb
@@ -0,0 +1,50 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students= []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name,  cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index.to_i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+def first_letter(students)
+  puts "What letter do the names you want to view begin with?"
+  letter = gets.chomp
+  names = []
+  students.each do |student|
+    names << student[:name]
+  end
+  names.each do |name|
+    if name.start_with?(letter)
+      puts name
+    end
+  end
+end
+#nothing happens until we call the methods
+students = input_students
+puts students
+print_header
+print(students)
+first_letter(students)
+print_footer(students)
