aeeb168b2619fbf10820586f81ca7c42c226ed3b
Ed Thomas <et1270@my.bristol.ac.uk>
2018-07-08 23:10:26 +0100

    Exercise 6

--- corrected.rb
+++ corrected.rb
@@ -64,12 +64,12 @@ def input_students
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    push_students(name, cohort)
+  file = File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(',')
+      push_students(name, cohort)
+    end
   end
-  file.close
   puts "Loaded students successfully!"
 end
 
@@ -104,14 +104,14 @@ end
 
 def save_students(filename = "students.csv")
   #open the file for writing
-  file = File.open(filename, "w")
-  #iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  file = File.open(filename, "w") do |file|
+    #iterate over the array of students
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
   puts "Student list saved!"
 end
 
